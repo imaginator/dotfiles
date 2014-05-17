d8dd4d542a5ea25b2d62d6f1fe099954fa3dc54e
imaginator <simon@imaginator.com>
2014-05-17 13:40:15 +0000

    More changes to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,15 +3,17 @@
 cd ~/dotfiles
 #git pull origin master
 
-for dotfile in  `find ~/dotfiles -not -name '.git' -not -name 'README.md' -not -name 'bootstrap.sh' -not -name ".DS_Store" -depth 1 -exec basename {} \;`
+for dotfile in  `find ~/dotfiles -maxdepth 1 ! \( -name ".git" -o -name "README.md" -o -name "bootstrap.sh" -o -name ".DS_Store" -o -name "dotfiles" -o -name ".gitignore" \)  -exec basename {} \;`
 do
-  echo "$dotfile to be replaced"
-  if [ ! -L ~/"$dotfile" ]
-    then
-    echo "$dotfile is not a symlink. Stopping."
-    exit 0
+  if [ -L ~/"$dotfile" ]
+  then
+    echo "$dotfile: is a symlink. Nothing to do"
+  elif [ -f ~/"$dotfile" ] || [ -d ~/"$dotfile" ]
+  then
+    echo "$dotfile: a file or directory already exists here. Remove so it can be automatically symlinkd"
+  else
+    ln -s ~/dotfiles/$dotfile ~/$dotfile
+    echo "$dotfile symlined into place"
   fi
-  unlink ~/$dotfile
-  ln -s ~/dotfiles/$dotfile ~/$dotfile
 done
 
