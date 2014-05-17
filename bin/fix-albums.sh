3481cbe93f408a0c1a343a63fb9765cc9e136c82
imaginator <simon@imaginator.com>
2014-05-17 10:05:24 +0000

    Remove some old skritpz.

--- bin/fix-albums.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Fix the photo albums
-set -x
-find ~simon/public_html/albums -type f -name .DS_Store    -ls -exec rm {} \;
-find ~simon/public_html/albums -type f -name ._dscn\*     -ls -exec rm {} \;
-find ~simon/public_html/albums -type f -name \*Thumbs\*   -ls -exec rm {} \;
-find ~simon/public_html/albums -type f -name ._\*         -ls -exec rm {} \;
-find ~simon/public_html/albums -type f                        -exec chmod 644 {} \;
-find ~simon/public_html/albums -type d                        -exec chmod 755 {} \;
