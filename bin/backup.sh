66efbf25a08472c3ed5630be9ef83cf238ebfe55
imaginator <simon@imaginator.com>
2014-08-01 09:06:23 +0000

    backup script changes

--- bin/backup.sh
+++ bin/backup.sh
@@ -5,8 +5,8 @@ partprobe
 #lvcreate --snapshot --size 50G --name bunkerRootSnapshot /dev/bunkerStore/bunkerRoot
 cryptsetup luksOpen /dev/disk/by-uuid/13571e34-f219-43b5-9dc2-bb3ad519e5a0  backup
 mount -o user_xattr /dev/mapper/backup /mnt/backup
-ionice -c 3 rsync --delete --inplace -aAvx /      /mnt/backup/mountpoints/slash --exclude=/var/spool/squid3/
-ionice -c 3 rsync --delete --inplace -aAvx /boot/ /mnt/backup/mountpoints/boot
+ionice -c 3 rsync --delete --inplace --no-whole-file -aAvx /      /mnt/backup/mountpoints/slash --exclude=/var/spool/squid3/ --exclude=srv/backups/timemachine/
+ionice -c 3 rsync --delete --inplace --no-whole-file -aAvx /boot/ /mnt/backup/mountpoints/boot
 umount /mnt/backup
 cryptsetup luksClose  backup
 sync
