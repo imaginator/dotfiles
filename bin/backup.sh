3f79f6acb68e4cec2cd8dce04931f8ca47aff0e9
Simon Tennant <simon@imaginator.com>
2020-12-09 16:05:08 +0000

    add backup script

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,12 +1,9 @@
-#!/bin/sh
-set -x
-lsblk
-#partprobe
-#lvcreate --snapshot --size 50G --name bunkerRootSnapshot /dev/bunkerStore/bunkerRoot
-cryptsetup luksOpen /dev/disk/by-uuid/13571e34-f219-43b5-9dc2-bb3ad519e5a0  backup || cryptsetup luksOpen /dev/disk/by-uuid/88710eaa-55b0-4070-bd26-c182d2d075f3  backup
-vgscan 
-# vgchange --activate y   backup-01
-mount -o user_xattr /dev/mapper/backup /mnt/backup
+#!/bin/bash
+set -xe
+cryptsetup luksOpen /dev/disk/by-uuid/13571e34-f219-43b5-9dc2-bb3ad519e5a0  backup-01 || cryptsetup luksOpen /dev/disk/by-uuid/88710eaa-55b0-4070-bd26-c182d2d075f3  backup-01
+#vgscan 
+#vgchange --activate y   backup-01
+mount -o user_xattr /dev/mapper/backup-01 /mnt/backup
 ionice -c 3 rsync --delete --delete-excluded --inplace --no-whole-file -aAvx /      /mnt/backup/mountpoints/slash --exclude=".snapshot/" --exclude=/var/spool/squid3/ --exclude=srv/backups/timemachine/ --exclude=/srv/video/Film --exclude=/srv/video/TV
 ionice -c 3 rsync --delete --inplace --no-whole-file -aAvx /boot/ /mnt/backup/mountpoints/boot  --exclude=".snapshot/"
 umount /mnt/backup
